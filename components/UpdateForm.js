925762a217e09ee52559258dc2551f4da5cd8a0a
roshan-gautam <roshan.gautam@mavs.uta.edu>
2021-11-11 14:54:42 -0600

    Added UpdateForm and updated account.js

---
+++ components/UpdateForm.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Keyboard,TouchableOpacity } from 'react-native';
+import firebase from 'firebase';
+
+export default class UpdateForm extends React.Component {
+    state = {name : "", phone: ""}
+    
+    handleNameChange=(text)=>{
+        this.setState({name:text})
+    }
+    handlePhoneChange=(text)=>{
+      this.setState({phone:text})
+    }
+  
+    handleSubmit = () => {
+        const userID = firebase.auth().currentUser.uid;
+        const dbRef = firebase.app().database().ref('/'+userID)
+        if (this.state.name && this.state.name.length > 0) {
+            dbRef.set({
+            FullName: this.state.name,
+            Phone: this.state.phone
+            }).then(()=>{
+            }).catch(e => console.log(e))
+            this.props.navigation.replace('Accounts')
+        }
+    }
+  render() {
+    
+    
+    return (
+      <View style={styles.container}>
+        
+          <Text style={styles.header}>Update your information</Text>
+          <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => this.props.navigation.replace('Update')}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Go back</Text>
+        </TouchableOpacity>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Your name"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+            onChangeText={text => this.handleNameChange(text)}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Phone number"
+            maxLength={20}
+            keyboardType='numeric'
+            onBlur={Keyboard.dismiss}
+            onChangeText={text => this.handlePhoneChange(text)}
+        />
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={this.handleSubmit}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  textInput: {
+    borderColor: '#CCCCCC',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "sienna",
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    marginBottom: 30,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "seashell",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
+})
--- screens/Account.js
+++ screens/Account.js
@@ -1,10 +1,11 @@
 import React, {Component, useEffect, useState} from "react";
-import { StyleSheet, Button, TextInput, Image, View, Alert, KeyboardAvoidingView } from "react-native";
+import { StyleSheet, Button, TextInput, Image, View, Alert, KeyboardAvoidingView, Text, TouchableOpacity } from "react-native";
 import { WebView } from 'react-native-webview';
 import { createStackNavigator } from "@react-navigation/stack";
 import firebase from "firebase/app";
 import ProfilePic from "../components/ProfilePic"
-
+import UpdateForm from "../components/UpdateForm"
+import {RootStack} from "./Login"
 
 const Browser = ({ navigation, route }) => {
   return (
@@ -22,69 +23,92 @@ const Browser = ({ navigation, route }) => {
 
 
 const Update = ({ navigation }) => {
-
-  const [nameText,setNameText] = useState('')
-  const [phoneText, setPhoneText] = useState('')
-  const userID = firebase.auth().currentUser.uid;
-  const dbRef = firebase.app().database().ref('/'+userID)
-  const onSubmitButtonPress = () => {
-  if (nameText && nameText.length > 0) {
-      dbRef.set({
-        FullName: nameText,
-        Phone: phoneText
-      }).then(()=>{
-        console.log("data updated")
-      }).catch(e => console.log(e))
-      navigation.replace('Accounts')
-    }
-  }
-
   return (    
-    <KeyboardAvoidingView behavior="padding" style={styles.container}>
-    <Button title="Go back" onPress={() => navigation.replace('Accounts')} />
-
-    <TextInput
-      style={styles.input}
-      placeholder="Name"
-      onChangeText={(userDisplay) => setNameText(userDisplay)}
-    />
-    <TextInput
-      keyboardType="numeric"
-      style={styles.input}
-      placeholder="Phone"
-      onChangeText={(phoneDisplay) => setPhoneText(phoneDisplay)}
-    />
-    <Button title="Change profile picture" onPress={() => navigation.navigate('ProfilePic')}/>
-    <Button title="Submit" onPress={onSubmitButtonPress}/>
+    <KeyboardAvoidingView style={styles.container}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigation.replace('Accounts')}
+      style={styles.appButtonContainer}
+    >
+    <Text style={styles.appButtonText}>Go Back</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigation.navigate('ProfilePic')}
+      style={styles.appButtonContainer}
+    >
+    <Text style={styles.appButtonText}>Change profile picture</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigation.navigate('UpdateForm')}
+      style={styles.appButtonContainer}
+    >
+    <Text style={styles.appButtonText}>Change your information</Text>
+    </TouchableOpacity>
+    
   </KeyboardAvoidingView>
   )
 }
 
 
 
-function handleSignOut() {
+function handleSignOut(navigation) { 
   firebase.auth().signOut()
   Alert.alert("Message", "Signout Successful.")
 }
 
 
 class Account extends Component{ 
-  state = {uri : ""}
+  state = {uri : "../assets/userimage.png", name : "", phone: ""}
+  componentDidMount(){
+    const userID = firebase.auth().currentUser.uid;
+    const dbRef = firebase.app().database().ref('/'+userID)
+    dbRef.once('value').then(snapshot=>{
+      this.setState({name: snapshot.val().FullName, phone: snapshot.val().Phone})
+    });
+  }
   render() {
     const pp = "/" + firebase.auth().currentUser.uid + "pp.jpg"
     const ref = firebase.storage().ref(pp);
-    const img_uri = ref.getDownloadURL().then(url => {this.setState({uri: url})}).catch(e=>{console.log(e)});
+    ref.getDownloadURL().then(url => {this.setState({uri: url})}).catch(e=>{console.log(e)});
   return (
-      <View style={styles.container}>
+      <View style={styles.buttonStyle}>
+        <Text style={styles.textStyle}>{this.state.name} </Text>
         <Image source={{uri: this.state.uri }} style={styles.image}></Image>
-        <Button  title="Ultimate Guide to Fishing"  size="sm" backgroundColor="#007bff" onPress={() => this.props.navigation.navigate('Browsers', { uri: 'https://kayakguru.com/guide-to-fishing/' })} />
-        <Button color='brown' title="Texas WildLife Fishing Rules and Regulations" onPress={() => this.props.navigation.navigate('Browsers', { uri: 'https://tpwd.texas.gov/fishboat/fish/' })} />
-        <Button color='black' title="update profile" onPress={() => this.props.navigation.navigate('Update')} />
-        <Button   title="Sign Out" onPress={() => handleSignOut()}  style={{color: 'red', marginTop: 10, padding: 10}} />
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => this.props.navigation.navigate('Update')}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Update Profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => this.props.navigation.navigate('Browsers', { uri: 'https://kayakguru.com/guide-to-fishing/' })}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Guide to Fishing</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => this.props.navigation.navigate('Browsers', { uri: 'https://tpwd.texas.gov/fishboat/fish/' })}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Rules and Regulations</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => handleSignOut(this.props.navigation)}
+          style={styles.appButtonContainer}
+        >
+        <Text style={styles.appButtonText}>Sign Out</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
 const Stack = createStackNavigator();
 
 export default function MyStack() {
@@ -94,6 +118,7 @@ export default function MyStack() {
       <Stack.Screen name="Browsers" component={Browser} />
       <Stack.Screen name="Update" component={Update} />
       <Stack.Screen name="ProfilePic" component={ProfilePic} />
+      <Stack.Screen name="UpdateForm" component={UpdateForm} />
     </Stack.Navigator>
   )
 }
@@ -103,26 +128,25 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 10,
-    paddingVertical: 10,
+    borderRadius: 100,
     paddingHorizontal: 12
 
   },
   button: {
-    flex: 0.15,
-    alignSelf: 'flex-end',
-    alignItems:'center',
-    backgroundColor: 'transparent',
-    padding: 10,
-    width: 120,
-
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
   },
   input: {
     borderWidth: 1,
     borderColor: 'black',
-    minWidth: 100,
+    minWidth: 200,
     marginTop: 20,
-    marginHorizontal: 20,
+    marginHorizontal: 10,
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 3,
@@ -134,7 +158,8 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 100,
     overflow: "hidden",
-    marginTop: 32
+    marginTop: 32,
+    marginBottom: 32
   },
 
   baseText: {
@@ -142,6 +167,56 @@ const styles = StyleSheet.create({
   },
   innerText: {
     color: 'red'
-  }
+  },
+  buttonStyle: {
+    marginTop:10,
+    marginLeft:10,
+    marginRight:10,
+    marginBottom:10,
+    borderWidth:2,
+    borderRadius:20,
+    borderColor:'sandybrown',
+    overflow:"hidden",
+    backgroundColor: 'slategray'
+  },
+  textStyle: {
+    marginTop: 10,
+    color: 'cornsilk',
+    textAlign: 'center', 
+    fontWeight: 'bold',
+    fontSize: 25,
+    backgroundColor: 'slategray',
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "sienna",
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    marginBottom: 30,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+
+    
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "seashell",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  formStyle: {
+    marginTop:10,
+    marginLeft:10,
+    marginRight:10,
+    marginBottom:10,
+    borderWidth:2,
+    borderRadius:20,
+    borderColor:'sandybrown',
+    overflow:"hidden",
+    backgroundColor: 'slategray'
+  },
 });
    
